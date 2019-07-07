b36bb1de43119dd2ea0db832e8474499d618214a
Costin F <costinfl@gmail.com>
2019-07-07 14:00:14 +0300

    added api_key.dart to hold the feeldy token

---
+++ lib/network/api_key.dart
@@ -0,0 +1,8 @@
+const String AUTHORIZATION_TOKEN =
+    '<FEEDLY_API_TOKEN_HERE>';
+const String AUTHORIZATION_HEADER = 'Bearer $AUTHORIZATION_TOKEN';
+
+const FEEDLY_HEADERS = {
+  "Accept": "application/json",
+  "Authorization": AUTHORIZATION_HEADER,
+};
